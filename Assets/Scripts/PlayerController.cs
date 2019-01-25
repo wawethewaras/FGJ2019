251d70febceddd3b14df8e2ba0c4a41c6755a100
Wawe <Wawe212@hotmail.fi>
2019-01-25 20:38:04 +0200

    New art files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRidigbody;
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private int movementSpeed;
+
+    void Start()
+    {
+        myRidigbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+        myRidigbody.velocity = movementInput.normalized * movementSpeed * Time.deltaTime;
+    }
+}

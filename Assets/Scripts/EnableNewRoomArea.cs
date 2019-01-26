d5877d31b93bc4ac917f19e106ed44c72b8485b3
Wawe <Wawe212@hotmail.fi>
2019-01-26 15:58:32 +0200

    More door enabling scripts

---
+++ Assets/Scripts/EnableNewRoomArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableNewRoomArea : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject myRoomEnabler;
+    [SerializeField]
+    private GameObject myRoomToEnable;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == myRoomEnabler)
+        {
+            myRoomToEnable.SetActive(true);
+        }
+    }
+}

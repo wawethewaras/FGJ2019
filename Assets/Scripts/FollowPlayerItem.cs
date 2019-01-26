d9416320327b42b3c9844540eec49d73233eb942
Wawe <Wawe212@hotmail.fi>
2019-01-26 13:16:13 +0200

    Improved player controller.
    Now behaves better than uuxo.

---
+++ Assets/Scripts/FollowPlayerItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerItem : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.SetParent(other.transform);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody myRidigbody;
@@ -11,15 +11,44 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private int movementSpeed;
 
+    public float MouseSensitivity;
+
     void Start()
     {
         myRidigbody = GetComponent<Rigidbody>();
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
-        myRidigbody.velocity = movementInput.normalized * movementSpeed * Time.deltaTime;
+        myRidigbody.MoveRotation(myRidigbody.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0)));
+        myRidigbody.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * movementSpeed *Time.deltaTime) + (transform.right * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime));
+
+
+        float rotationX = Head.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+
+        rotationY -= Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        Head.transform.localEulerAngles = new Vector3(rotationY, rotationX, 0);
     }
+
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    public float viewRange = 60.0f;
+    float rotationY = 0F;
+
+    public GameObject Head;
+
+
+
+  
 }
